efa0055fdff72022bf1b17c82e62d6a812bee3c7
DESKTOP-E3B72DN\flavio <flaviob.san@gmail.com>
2017-10-15 11:07:05 -0200

    fixed wall jump bug - when disable input after succesfull wall jump messed with CanJump if no intup is made,, solved by making a axis input bypass boolean

---
+++ MilhoMan/Source/MilhoMan/MyPaperCharacter.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "MyPaperCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MILHOMAN_API AMyPaperCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
