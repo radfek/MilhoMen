d51d82c4f84ae3b5c9a6829c5fcb7dd3bcf83e81
DESKTOP-E3B72DN\flavio <flaviob.san@gmail.com>
2017-11-02 16:09:12 -0200

    4.18 upgrade modelo do level 1 no tile map

---
+++ MilhoMan 4.18/Source/MilhoMan/MilhoManGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MilhoManGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MILHOMAN_API AMilhoManGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
